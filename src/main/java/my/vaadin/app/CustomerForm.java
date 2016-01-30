a274a2e9b4e9551f186a6339793f0f08f6ff906a
Petri Heinonen <petri@vaadin.com>
2016-01-29 16:18:51 -0800

    Removed seralId from CustomerForm

--- src/main/java/my/vaadin/app/CustomerForm.java
+++ src/main/java/my/vaadin/app/CustomerForm.java
@@ -7,20 +7,20 @@ import my.vaadin.app.service.CustomerService;
 import my.vaadin.app.service.CustomerServiceImpl;
 
 public class CustomerForm extends CustomerFormDesign {
-
+	
 	private CustomerService service = CustomerServiceImpl.getInstance();
 	private Customer customer;
 	private MyUI parent;
-
+	
 	public CustomerForm(MyUI myUI) {
 		this.parent = myUI;
-		save.addClickListener(e -> this.save());
-		delete.addClickListener(e -> this.delete());
+		save.addClickListener(e->this.save());
+		delete.addClickListener(e->this.delete());
 		status.removeAllItems(); // Remove demo data assigned by Designer
 		status.addItems(CustomerStatus.values());
 		save.setClickShortcut(KeyCode.ENTER);
 	}
-
+	
 	public void setCustomer(Customer customer) {
 		this.customer = customer;
 		// Show delete button for only customers already in the database
@@ -29,7 +29,7 @@ public class CustomerForm extends CustomerFormDesign {
 		setVisible(true);
 		firstName.selectAll();
 	}
-
+	
 	private void delete() {
 		service.delete(customer);
 		parent.updateList();
@@ -39,5 +39,5 @@ public class CustomerForm extends CustomerFormDesign {
 		service.save(customer);
 		parent.updateList();
 	}
-
+	
 }
